a7bf70d277009c30af22982225bdf3bc53ce985b
zineb essahri <raja.essahri@etu.uae.ac.ma>
2022-04-24 14:25:44 +0000

    [Update] - load image

--- Script1.py
+++ Script1.py
@@ -1,5 +1,11 @@
 import math
 import cv2
-import cvzone
-from cvzone.ColorModule import ColorFinder
-import numpy as np
+cap = cv2.VideoCapture('Videos/vid (1).mp4')
+while True:
+    #sucess, img = cap.read()
+    img = cv2.imread("assets/images/Ball.png")
+    img = cv2.resize(img, (0, 0), None, 0.7, 0.7)
+    cv2.imshow("Image", img)
+    cv2.waitKey(50)
+
+
